f32987871374b101a103f9fd59b1971ce460619a
Raoul Kent <raoulkent@gmail.com>
2024-12-23 12:38:21 +0100

    add and use definitions for HRM mod taps

--- keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
+++ keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
@@ -18,13 +18,28 @@ void matrix_scan_user(void) {
   achordion_task();
 }
 
+/* Some useful mod tap keys, used for homerow mods
+ * These follow the format of MT(MOD, KEY), used by achordion
+ * For example, MT_A is a mod tap key that sends LCTL when held and A when tapped
+ * They are implemented in the CAGS order (Ctrl, Alt, Gui, Shift) for bindings in MacOS
+ * You can read more about these homerow mods here: https://precondition.github.io/home-row-mods
+ */
+#define MT_A MT(MOD_LCTL, KC_A)
+#define MT_R MT(MOD_LALT, KC_R)
+#define MT_S MT(MOD_LGUI, KC_S)
+#define MT_T MT(MOD_LSFT, KC_T)
+#define MT_N MT(MOD_RSFT, KC_N)
+#define MT_E MT(MOD_RGUI, KC_E)
+#define MT_I MT(MOD_RALT, KC_I)
+#define MT_O MT(MOD_RCTL, KC_O)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_fun(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LCTL, MT_A,    MT_R,    MT_S,    MT_T,    KC_G,                                KC_M,    MT_N,    MT_E,    MT_I,    MT_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LGUI,   KC_LALT,   KC_LGUI, KC_ENT, KC_DEL,           KC_BSPC,  KC_SPC,  MO(1), KC_LBRC,  KC_RBRC,
                                             KC_HOME, KC_END,    KC_ESC,           KC_GRAVE,  KC_CAPS,  KC_APP
