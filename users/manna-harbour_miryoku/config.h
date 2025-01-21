acd0b63b81d50ae249639e3bd6559c529bce52a5
Raoul Kent <raoulkent@gmail.com>
2025-01-21 17:07:41 +0100

    wip

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -12,7 +12,7 @@
 #define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
@@ -20,7 +20,7 @@
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-#define AUTO_SHIFT_NO_SETUP
+// #define AUTO_SHIFT_NO_SETUP
 
 // Mouse key speed and acceleration.
 #undef MOUSEKEY_DELAY
