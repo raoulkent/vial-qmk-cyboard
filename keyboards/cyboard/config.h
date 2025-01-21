dea6e1f8448364b56b742b097c2fcaeaad7362d9
Raoul Kent <raoulkent@gmail.com>
2025-01-21 19:34:45 +0100

    re-enable achordion streak, tapping term and permissive hold

--- keyboards/cyboard/config.h
+++ keyboards/cyboard/config.h
@@ -55,9 +55,9 @@
 #define PMW3360_LIFTOFF_DISTANCE 0x02 //default 0x02, 2mm liftoff distance
 
 // // Achordion settings
-// #define ACHORDION_STREAK
+#define ACHORDION_STREAK
 
 // // Tap-hold configuration for home row mods.
-// #define TAPPING_TERM 175
-// #define PERMISSIVE_HOLD
+#define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
 // #define QUICK_TAP_TERM_PER_KEY
